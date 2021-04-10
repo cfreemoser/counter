d51d57fd342a4907373a4c2f1cb16ceb9099c03f
Cem Philipp Freimoser <cem.freimoser@egym.com>
2021-04-10 15:26:17 +0200

    bloc first try

---
+++ lib/counter_observer.dart
@@ -0,0 +1,11 @@
+import "package:bloc/bloc.dart";
+
+class CounterObserver extends BlocObserver {
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print('${bloc.runtimeType} $transition');
+  }
+
+}
